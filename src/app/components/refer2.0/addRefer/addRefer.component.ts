e955648b5ec86bf9a53958d82fcb391054ea7d5e
Ivan9808 <herleon98@gmail.com>
2020-10-23 14:45:11 -0500

    Servicio para referir CDT

--- src/app/components/refer2.0/addRefer/addRefer.component.ts
+++ src/app/components/refer2.0/addRefer/addRefer.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MaskTypeInterface, MaskTypes } from 'src/app/interface/maskType.interfaces';
+import { PersonRefered } from 'src/app/model/refered.model';
+import { ApiGeneralService } from 'src/app/service/common/api-general.service';
+import { LoggerService } from 'src/app/service/common/logger.service';
+import { ReferidoService } from 'src/app/service/referido.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-addRefer',
@@ -13,55 +18,62 @@ export class AddReferComponent implements OnInit {
   public maskTypeNames: MaskTypeInterface = MaskTypes.find(m => m.type == "names"); // Indica el tipo de mascara a   utilizar en el input-text
   public maskTypeNumber: MaskTypeInterface = MaskTypes.find(m => m.type == "number"); // Indica el tipo de mascara a   utilizar en el 
   public listOffices = [
-  'ARMENIA',
-  'BARRANQUILLA - CALLE 72',
-  'BARRANQUILLA - CENTRO',
-  'BARRANQUILLA - PRADO',
-  'BOGOTA - 7 DE AGOSTO',
-  'BOGOTA - AVENIDA AMÉRICAS',
-  'BOGOTA - AVENIDA CHILE',
-  'BOGOTA - CALLE 80',
-  'BOGOTA - CARRERA 10',
-  'BOGOTA - CARVAJAL',
-  'BOGOTA - CENTRO CALLE 22',
-  'BOGOTA - CHAPINERO',
-  'BOGOTA - CHICO',
-  'BOGOTA - CONTADOR',
-  'BOGOTA - FONTIBON',
-  'BOGOTA - KENNEDY',
-  'BOGOTA - LA ESMERALDA',
-  'BOGOTA - RESTREPO',
-  'BOGOTA - TOBERIN',
-  'BOGOTA - UNICENTRO',
-  'BOGOTA - VENECIA',
-  'BUCARAMANGA - CABECERA',
-  'BUCARAMANGA - CAÑAVERAL',
-  'BUCARAMANGA - PASEO DEL COMERCIO',
-  'CALI - ALAMEDA',
-  'CALI - AVENIDA SEXTA NORTE',
-  'CALI - CENTRO CARRERA 3',
-  'CALI - HOLGUINES',
-  'CARTAGENA - LA MATUNA',
-  'CUCUTA - AVENIDA CERO',
-  'IBAGUE - CENTRO CALLE 15',
-  'IPIALES',
-  'ITAGUI',
-  'MANIZALES - MULTICENTRO',
-  'MEDELLIN - ENVIGADO',
-  'MEDELLIN - JUNIN',
-  'MEDELLIN - LAURELES',
-  'MEDELLIN - POBLADO',
-  'MONTERIA - PARQUE SIMON BOLIVAR',
-  'NEIVA - CENTRO CALLE 7',
-  'PASTO - CENTRO CALLE 19',
-  'PEREIRA - AV CIRCUNVALAR',
-  'PEREIRA - PLAZA BOLIVAR',
-  'SAN GIL',
-  'TUNJA',
-  'ZAPATOCA',
-  'ASIGNAR POR GEOREFERENCIACIÓN'
-]
-  constructor() { }
+    '',
+    'ARMENIA',
+    'BARRANQUILLA - CALLE 72',
+    'BARRANQUILLA - CENTRO',
+    'BARRANQUILLA - PRADO',
+    'BOGOTA - 7 DE AGOSTO',
+    'BOGOTA - AVENIDA AMÉRICAS',
+    'BOGOTA - AVENIDA CHILE',
+    'BOGOTA - CALLE 80',
+    'BOGOTA - CARRERA 10',
+    'BOGOTA - CARVAJAL',
+    'BOGOTA - CENTRO CALLE 22',
+    'BOGOTA - CHAPINERO',
+    'BOGOTA - CHICO',
+    'BOGOTA - CONTADOR',
+    'BOGOTA - FONTIBON',
+    'BOGOTA - KENNEDY',
+    'BOGOTA - LA ESMERALDA',
+    'BOGOTA - RESTREPO',
+    'BOGOTA - TOBERIN',
+    'BOGOTA - UNICENTRO',
+    'BOGOTA - VENECIA',
+    'BUCARAMANGA - CABECERA',
+    'BUCARAMANGA - CAÑAVERAL',
+    'BUCARAMANGA - PASEO DEL COMERCIO',
+    'CALI - ALAMEDA',
+    'CALI - AVENIDA SEXTA NORTE',
+    'CALI - CENTRO CARRERA 3',
+    'CALI - HOLGUINES',
+    'CARTAGENA - LA MATUNA',
+    'CUCUTA - AVENIDA CERO',
+    'IBAGUE - CENTRO CALLE 15',
+    'IPIALES',
+    'ITAGUI',
+    'MANIZALES - MULTICENTRO',
+    'MEDELLIN - ENVIGADO',
+    'MEDELLIN - JUNIN',
+    'MEDELLIN - LAURELES',
+    'MEDELLIN - POBLADO',
+    'MONTERIA - PARQUE SIMON BOLIVAR',
+    'NEIVA - CENTRO CALLE 7',
+    'PASTO - CENTRO CALLE 19',
+    'PEREIRA - AV CIRCUNVALAR',
+    'PEREIRA - PLAZA BOLIVAR',
+    'SAN GIL',
+    'TUNJA',
+    'ZAPATOCA',
+    'ASIGNAR POR GEOREFERENCIACIÓN'
+  ]
+  public flagService: number = 1; //Variable para saber que servicio se consumirá. (Por defecto se deja en CDT.)
+  public selectCdt: boolean = true; // (Por defecto se deja en CDT.)
+  public selectTcd: boolean = false;
+  public rspSendInvitation: any;
+  public isDisabled = true;
+  constructor(private log: LoggerService, private apiService: ApiGeneralService, private PersonRefered: PersonRefered) {
+  }
 
   ngOnInit() {
     this.loadOffices();
@@ -69,15 +81,101 @@ export class AddReferComponent implements OnInit {
       documentPerson: new FormControl("", [Validators.required]),
       office: new FormControl("", Validators.required),
     })
-    
+    document.getElementById('im1').setAttribute("src", "./assets/img/cdtblue.png");
+    document.getElementById('p1').style.color = '#72787A';
   }
   get documentPerson() {
     return this.formRefer.get('documentPerson');
   }
-
+  get office(){
+    return this.formRefer.get('office');
+  }
   get f() { return this.formRefer }
 
-  public loadOffices() {
+  public consumeService() {
+    if (this.flagService == 1) {
+      this.addReferCdtService();
+    }
+    if (this.flagService == 2) {
+      // this.addReferTdcService();
+    }
+  }
+  public addReferCdtService() {
+    this.PersonRefered.token = sessionStorage.getItem('rspToken');
+    this.PersonRefered.document = this.documentPerson.value;
+    this.PersonRefered.productRefer = this.selectCdt == true ? 'CDT' : '';
+    this.PersonRefered.office = (document.getElementById('selectOffice') as HTMLInputElement).value;
+    console.log('office: ' + this.PersonRefered.office);
+    try {
+      this.apiService.invokePostRequest<PersonRefered, any>(
+        // environment.endPointAddReferCdt,
+        "",
+        this.PersonRefered,
+        (rsp: any) => {
+          this.callbackSendInvitationRefer();
+        }
+      );
+    } catch (err) {
+      this.log.error(this, "Error consumiendo el servicio agregar/registro persona: " + err);
+    }
+  }
+
+  private callbackSendInvitationRefer(): any {
+    this.rspSendInvitation = this.apiService.response;
+    if (this.rspSendInvitation[0].status == true) {
+      // this.graphicalDataService();
+      // this.getRefferalService();
+      alert(this.rspSendInvitation[0].response);
+      // (document.getElementById('add-document-referido') as HTMLInputElement).value = "";
+      // (document.getElementById('tarjeta-credito') as HTMLInputElement).checked == false;
+      // (document.getElementById('selectOffice') as HTMLInputElement).value = "";
+      // (document.getElementById('newRefer') as HTMLInputElement).checked == false;
+    } else if (this.rspSendInvitation[0].status == false) {
+      alert(this.rspSendInvitation[0].response);
+    }
+    return this.rspSendInvitation;
+  }
+
+  public selectProduct(event) {
+    this.clean()
+    let element = event.currentTarget as HTMLElement;
+    let formcdt = document.getElementById('formcdt');
+    let formtcd = document.getElementById('formtcd');
+    if (element.id == "im1") {
+      element.setAttribute("src", "./assets/img/cdtblue.png");
+      document.getElementById('p1').style.color = '#10133F';
+      formcdt.style.display = 'flex';
+      formtcd.style.display = 'none';
+      this.selectCdt = true;
+      this.selectTcd = false;
+
+    }
+    if (element.id == "im2") {
+      element.setAttribute("src", "./assets/img/tcdblue.png");
+      document.getElementById('p2').style.color = '#10133F';
+      formcdt.style.display = 'none';
+      formtcd.style.display = 'flex';
+      this.selectCdt = false;
+      this.selectTcd = true;
+    }
+
+    if (element.id == "im3") {
+      element.setAttribute("src", "./assets/img/libranzablue.png");
+      document.getElementById('p3').style.color = '#10133F';
+    }
+  }
+  public validateForm(){
+    let office = (document.getElementById('selectOffice') as HTMLSelectElement)
+    if (this.flagService == 1) {
+      if (this.documentPerson.value !="" && office.value !="") {
+        this.isDisabled = !this.isDisabled;
+      } else{
+        this.isDisabled = true;
+      }
+    }
+ 
+  }
+  private loadOffices() {
     let slcCities = (document.getElementById('selectOffice') as HTMLSelectElement);
     for (let i = 0; i < this.listOffices.length; i++) {
       let dv = document.createElement("option");
@@ -87,29 +185,13 @@ export class AddReferComponent implements OnInit {
       slcCities.appendChild(dv);
     }
   }
-  public selectProduct(event){
-    this.clean()
-    let element = event.currentTarget as HTMLElement;
-    if (element.id == "im1") {
-        element.setAttribute("src", "./../../../../assets/img/cdtblue.png");
-        document.getElementById('p1').style.color = '#10133F';
-    }
-    if (element.id == "im2") {
-      element.setAttribute("src", "./../../../../assets/img/tcdblue.png");
-      document.getElementById('p2').style.color = '#10133F';
-    }
-    if (element.id == "im3") {
-      element.setAttribute("src", "./../../../../assets/img/libranzablue.png");
-      document.getElementById('p3').style.color = '#10133F';
-    }
-    }
-  private clean(){
-    document.getElementById('im1').setAttribute("src","./../../../../assets/img/cdt.png");
-    document.getElementById('im2').setAttribute("src","./../../../../assets/img/tarjeta-de-credito.png");
-    document.getElementById('im3').setAttribute("src","./../../../../assets/img/libranza.png");
+  private clean() {
+    document.getElementById('im1').setAttribute("src", "./assets/img/cdt.png");
+    document.getElementById('im2').setAttribute("src", "./assets/img/tarjeta-de-credito.png");
+    document.getElementById('im3').setAttribute("src", "./assets/img/libranza.png");
     document.getElementById('p1').style.color = '#72787A';
     document.getElementById('p2').style.color = '#72787A';
     document.getElementById('p3').style.color = '#72787A';
+  }
 
-  }
-  }
+}
